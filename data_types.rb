cf4a7fbe2cf4910e05949c722d7823644b7ae033
rippr-r <136972470+rippr-r@users.noreply.github.com>
2026-05-05 20:57:13 -0400

    Delete data_types.rb

--- data_types.rb
+++
@@ -1,7 +0,0 @@
-name = "Mike" # Stored as a string
-age = 30 # Stored as an integer | Can be negative
-gpa = 3.2 # Stored as a float by default | Can be negative
-
-isMale = true # Stored as a boolean
-isTall = false 
-flaws = nil # Stored as nil | Represents the absence of a value
