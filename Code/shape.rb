d0becf5366b19406fc305492793885148c743529
rippr-r <136972470+rippr-r@users.noreply.github.com>
2026-05-05 20:56:07 -0400

    Add files via upload

---
+++ Code/shape.rb
@@ -0,0 +1,12 @@
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+
+# Puts automatically puts a new line after each print statement
+# Print does not
+
+print "   /|\n"
+print "  / |\n"
+print " /  |\n"
+print "/___|"
