3962093e7e5196c44b07dbce95775caec76cf11a
rippr-r <136972470+rippr-r@users.noreply.github.com>
2026-05-05 20:58:08 -0400

    Delete user_input.rb

--- user_input.rb
+++
@@ -1,8 +0,0 @@
-puts "Enter your name: "
-name = gets
-puts "Hello " + name + "Awh..Im on a new line..."
-
-# To get rid of the NL character
-puts "Enter your name: "
-name = gets.chomp # .chomp removes the newline character
-puts "Hello " + name + "! Now im not!"
